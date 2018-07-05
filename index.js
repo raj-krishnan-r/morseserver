5c1df45eb93f9ddcaf3dab5ff80c25b5385a1be7
Raj Krishnan R <rkrishnan1993@live.com>
2018-07-05 22:14:57 +0530

    hello

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+
+var port  = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080;
+var ip =  process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost';
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port,ip,null function(){
+  console.log('listening on *: '+port);
 });
     
